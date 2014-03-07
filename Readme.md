b2a77959868e2c68bb70e4856bd1d7d667ed1f1a
Guillermo Rauch <rauchg@gmail.com>
2014-03-07 10:54:19 -0600

    initial commit

---
+++ Readme.md
@@ -0,0 +1,43 @@
+
+# socket.io-emitter
+
+`socket.io-emitter` allows you to communicate with socket.io servers
+easily from non-socket.io processes.
+
+## How to use
+
+```js
+var io = require('socket.io-emitter')();
+setInterval(function(){
+  io.emit('time', new Date);
+}, 5000);
+```
+
+## API
+
+### Emitter(client[, opts])
+
+`client` is a [node_redis](https://github.com/mranney/node_redis)
+compatible client that has been initialized with the `return_buffers`
+option set to `true`. This argument is optional.
+
+The following options are allowed:
+
+- `key`: the name of the key to pub/sub events on as prefix (`socket.io`)
+- `host`: host to connect to redis on (`localhost`)
+- `port`: port to connect to redis on (`6379`)
+
+### Emitter(opts)
+
+If you don't want to supply a redis client object, and want
+`socket.io-emitter` to intiialize one for you, make sure to supply the
+`host` and `port` options.
+
+### Emitter#to(room:String):Emitter
+### Emitter#in(room:String):Emitter
+
+Specifies a specific `room` that you want to emit to.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,111 @@
+
+/**
+ * Module dependencies.
+ */
+
+var client = require('redis').createClient;
+var parser = require('socket.io-parser');
+var hasBin = require('has-binary-data');
+var msgpack = require('msgpack').pack;
+var debug = require('debug')('socket.io-emitter');
+
+/**
+ * Module exports.
+ */
+
+module.exports = Emitter;
+
+/**
+ * Flags.
+ *
+ * @api public
+ */
+
+var flags = [
+  'json',
+  'volatile',
+  'broadcast'
+];
+
+/**
+ * Socket.IO redis based emitter.
+ *
+ * @param {Object} redis client (optional)
+ * @param {Object} options
+ * @api public
+ */
+
+function Emitter(redis, opts){
+  if (!(this instanceof Emitter)) return new Emitter(redis, opts);
+  opts = opts || {};
+
+  if (redis && !redis.hset) {
+    opts = redis;
+    redis = null;
+  }
+
+  if (!redis) {
+    if (!opts.host) throw new Error('Missing redis `host`');
+    if (!opts.port) throw new Error('Missing redis `port`');
+    redis = client(opts.port, opts.host);
+  }
+
+  this.redis = redis;
+  this.key = (opts.key || 'socket.io') + '#emitter';
+
+  this._rooms = [];
+  this._flags = {};
+}
+
+/**
+ * Apply flags from `Socket`.
+ */
+
+flags.forEach(function(flag){
+  Emitter.prototype.__defineGetter__(flag, function(){
+    debug('flag %s on', flag);
+    this._flags[flag] = true;
+    return this;
+  });
+});
+
+/**
+ * Limit emission to a certain `room`.
+ *
+ * @param {String} room
+ */
+
+Emitter.prototype.in =
+Emitter.prototype.to = function(room){
+  if (!~this._rooms.indexOf(room)) {
+    debug('room %s', room);
+    this._rooms.push(room);
+  }
+  return this;
+};
+
+/**
+ * Send the packet.
+ *
+ * @api private
+ */
+
+Emitter.prototype.emit = function(){
+  // packet
+  var args = Array.prototype.slice.call(arguments);
+  var packet = {};
+  packet.type = hasBin(args) ? parser.BINARY_EVENT : parser.EVENT;
+  packet.data = args;
+
+  // publish
+  this.redis.publish(this.key, msgpack([packet, {
+    rooms: this._rooms,
+    flags: this._flags
+  }]));
+
+  // reset state
+  this._rooms = [];
+  this._flags = {};
+
+  return this;
+};
